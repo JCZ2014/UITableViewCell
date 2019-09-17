7db2fa5faf6f104754687407390e318b2510400c
零开 <326980725@qq.com>
2019-09-17 21:41:22 +0800

    develop测试

--- UITableViewCell-master/AppDelegate.m
+++ UITableViewCell-master/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"develop测试");
     return YES;
 }
 
